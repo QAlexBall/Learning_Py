eb2f41046953ad367eb2cb653afc1817f81170a6
QAlexBall <zhuderen12345@icloud.com>
2018-05-07 15:08:05 +0800

    update

--- Three_Part_Moudule/Pandas/DataFrame_pandas.py
+++ Three_Part_Moudule/Pandas/DataFrame_pandas.py
@@ -63,3 +63,82 @@ five   2002  Nevada  2.9  NaN
 six    2003  Nevada  3.2  NaN
 Index(['year', 'state', 'pop', 'debt'], dtype='object')
 '''
+
+# 通过类似字典标记的方法或属性的方式，可以将DataFrame的列获取为一个Series
+print(frame2['state'])
+print(frame2.year)
+'''
+output:
+one        Ohio
+two        Ohio
+three      Ohio
+four     Nevada
+five     Nevada
+six      Nevada
+Name: state, dtype: object
+one      2000
+two      2001
+three    2002
+four     2001
+five     2002
+six      2003
+Name: year, dtype: int64
+'''
+# 列可以通过赋值的方式进行修改.
+frame2['debt'] = 16.5
+print(frame2)
+'''
+output:
+       year   state  pop  debt
+one    2000    Ohio  1.5  16.5
+two    2001    Ohio  1.7  16.5
+three  2002    Ohio  3.6  16.5
+four   2001  Nevada  2.4  16.5
+five   2002  Nevada  2.9  16.5
+six    2003  Nevada  3.2  16.5
+'''
+import numpy as np
+frame2['debt'] = np.arange(6.)
+print(frame2)
+'''
+output:
+       year   state  pop  debt
+one    2000    Ohio  1.5   0.0
+two    2001    Ohio  1.7   1.0
+three  2002    Ohio  3.6   2.0
+four   2001  Nevada  2.4   3.0
+five   2002  Nevada  2.9   4.0
+six    2003  Nevada  3.2   5.0
+'''
+
+frame2['eastern'] = frame2.state == 'Ohio'
+print(frame2)
+'''
+output:
+       year   state  pop  debt  eastern
+one    2000    Ohio  1.5   0.0     True
+two    2001    Ohio  1.7   1.0     True
+three  2002    Ohio  3.6   2.0     True
+four   2001  Nevada  2.4   3.0    False
+five   2002  Nevada  2.9   4.0    False
+six    2003  Nevada  3.2   5.0    False
+'''
+# del方法可以删除列
+del frame2['eastern']
+print(frame2.columns)
+'''
+output:
+Index(['year', 'state', 'pop', 'debt'], dtype='object')
+'''
+
+'''
+注意: 通过索引方式返回的列只是响应数据的视图, 并不是副本.
+因此,对返回Series所做的任何就地修改全都会反应到源DataFrame上.
+通过copy方法即可指定复制列
+'''
+# 另一种常见数据形式是嵌套字典
+pop = {'Nevada': {2001: 2.4, 2002: 2.9}, 
+		'Ohio': {2000: 1.5, 2001: 1.7, 2002: 3.6}}
+# 如果嵌套字典传给DataFrame,pandas就会被解释: 外层字典的键作为列,内层键则作为行索引
+frame3 = pd.DataFrame(pop)
+print(frame3)
