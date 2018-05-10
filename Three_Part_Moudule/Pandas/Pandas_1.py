4ebffbff56aad10c72091e977f93e550edb3e519
QAlexBall <zhuderen12345@icloud.com>
2018-05-10 16:13:59 +0800

    update

--- Three_Part_Moudule/Pandas/Pandas_1.py
+++ Three_Part_Moudule/Pandas/Pandas_1.py
@@ -83,3 +83,208 @@ b        NaN   NaN         NaN
 c        4.0   NaN         5.0
 d        7.0   NaN         8.0
 '''
+
+# 丢弃指定轴上的项
+
+obj = pd.Series(np.arange(5.), index=['a', 'b', 'c', 'd', 'e'])
+print(obj)
+'''
+a    0.0
+b    1.0
+c    2.0
+d    3.0
+e    4.0
+dtype: float64
+'''
+
+new_obj = obj.drop('c')
+print(new_obj)
+'''
+output:
+a    0.0
+b    1.0
+d    3.0
+e    4.0
+dtype: float64
+'''
+
+print(obj.drop(['d', 'c']))
+'''
+output:
+a    0.0
+b    1.0
+e    4.0
+dtype: float64
+'''
+
+# 对于DataFrame,可以删除任意轴上的索引值.
+data = pd.DataFrame(np.arange(16).reshape((4, 4)), 
+					index=['Ohio', 'Colorado', 'Utah', 'New York'],
+					columns=['one', 'two', 'three', 'four'])
+print(data)
+'''
+          one  two  three  four
+Ohio        0    1      2     3
+Colorado    4    5      6     7
+Utah        8    9     10    11
+New York   12   13     14    15
+'''
+print(data.drop(['Colorado', 'Ohio']))
+'''
+          one  two  three  four
+Utah        8    9     10    11
+New York   12   13     14    15
+'''
+
+# 通过传递axis=1或axis='columns'可以删除列的值:
+print(data.drop('two', axis=1))
+print(data.drop(['two', 'four'], axis='columns'))
+'''
+output:
+          one  three  four
+Ohio        0      2     3
+Colorado    4      6     7
+Utah        8     10    11
+New York   12     14    15
+          one  three
+Ohio        0      2
+Colorado    4      6
+Utah        8     10
+New York   12     14
+'''
+
+# 许多函数,如drop,会修改Series或DataFrame的大小或形状,可以就地修改对象,不会返回新的对象
+obj.drop('c', inplace=True)
+print(obj)
+'''
+output:
+a    0.0
+b    1.0
+d    3.0
+e    4.0
+dtype: float64
+'''
+
+# 索引,选取和过滤
+# Series索引(obj[...])的工作方式类似于Numpy数组的索引,只不过Series的索引值不只是整数
+obj = pd.Series(np.arange(4.), index=['a', 'b', 'c', 'd'])
+print(obj)
+'''
+output:
+a    0.0
+b    1.0
+c    2.0
+d    3.0
+dtype: float64
+'''
+
+print(obj['b']) # output: 1.0
+print(obj[1]) # output: 1.0
+print(obj[2:4])
+'''
+output:
+c    2.0
+d    3.0
+dtype: float64
+'''
+
+print(obj[['b', 'a', 'd']])
+'''
+output:
+b    1.0
+a    0.0
+d    3.0
+dtype: float64
+'''
+
+print(obj[[1, 3]])
+'''
+output:
+b    1.0
+d    3.0
+dtype: float64
+'''
+
+print(obj[obj < 2])
+'''
+output:
+a    0.0
+b    1.0
+dtype: float64
+'''
+
+# 利用标签的切片运算与普通的Python切片运算不同,其末端是包含的
+print(obj['b':'d'])
+'''
+output:
+b    1.0
+c    2.0
+d    3.0
+dtype: float64
+'''
+
+# 用切片可以对Series的部分进行设置
+obj['b':'c'] = 5
+print(obj)
+'''
+output:
+a    0.0
+b    5.0
+c    5.0
+d    3.0
+dtype: float64
+'''
+
+print(data)
+print(data['two'])
+'''
+output:
+          one  two  three  four
+Ohio        0    1      2     3
+Colorado    4    5      6     7
+Utah        8    9     10    11
+New York   12   13     14    15
+Ohio         1
+Colorado     5
+Utah         9
+New York    13
+Name: two, dtype: int64
+'''
+print(data[['three', 'one']])
+'''
+output:
+          three  one
+Ohio          2    0
+Colorado      6    4
+Utah         10    8
+New York     14   12
+'''
+
+print(data[:2])
+print(data[data['three'] > 5])
+print(data < 5)
+'''
+output:
+          one  two  three  four
+Ohio        0    1      2     3
+Colorado    4    5      6     7
+          one  two  three  four
+Colorado    4    5      6     7
+Utah        8    9     10    11
+New York   12   13     14    15
+            one    two  three   four
+Ohio       True   True   True   True
+Colorado   True  False  False  False
+Utah      False  False  False  False
+New York  False  False  False  False
+'''
+data[data < 5] = 0
+print(data)
+'''
+output:
+          one  two  three  four
+Ohio        0    0      0     0
+Colorado    0    5      6     7
+Utah        8    9     10    11
+New York   12   13     14    15
+'''
