52218414de8c87e8f993c0ab706cfb6891ec095a
QAlexBall <zhuderen12345@icloud.com>
2018-09-05 23:50:33 +0800

    add aiohttp test

--- Python_Questions/test.py
+++ Python_Questions/test.py
@@ -1,9 +1,55 @@
-def foo(*args, **kwargs):
-    print('args = ', args)
-    print('kwargs = ', kwargs)
-    print('-----------------')
+# def foo(*args, **kwargs):
+#     print('args = ', args)
+#     print('kwargs = ', kwargs)
+#     print('-----------------')
  
-foo(1, 2, 3, 4)
-foo(a=1, b=2, c=3)
-foo(1, 2, 3, 4, a=1, b=2, c=3)
-foo('a', 1, None, a=1, b='2', c=3)
+# foo(1, 2, 3, 4)
+# foo(a=1, b=2, c=3)
+# foo(1, 2, 3, 4, a=1, b=2, c=3)
+# foo('a', 1, None, a=1, b='2', c=3)
+# Client
+import aiohttp
+import asyncio
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'http://python.org')
+        print(html)
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
+# Server
+from aiohttp import web
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+async def wshandle(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == web.WSMsgType.text:
+            await ws.send_str("Hello, {}".format(msg.data))
+        elif msg.type == web.WSMsgType.binary:
+            await ws.send_bytes(msg.data)
+        elif msg.type == web.WSMsgType.close:
+            break
+
+    return ws
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/echo', wshandle),
+                web.get('/{name}', handle)])
+
+web.run_app(app)
