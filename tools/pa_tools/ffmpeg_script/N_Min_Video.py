d871429c3d77a1bef563bb9da0fe4e1da513cfb8
chriszhu <chriszhu@motherapp.com>
2019-08-30 14:29:06 +0800

    update N_Min_Video.py

--- tools/pa_tools/ffmpeg_script/N_Min_Video.py
+++ tools/pa_tools/ffmpeg_script/N_Min_Video.py
@@ -20,9 +20,10 @@ def get_N_Min_Video(N, video):
     video_N_second = get_video_length(video)
     start_cut = 0
     while video_N_second > start_cut:
+        #  cmd = 'ffmpeg -y -ss 00:00:00 -t 00:23:00 -i 401-2.mp4 -vcodec copy -acodec copy -c:a aac 401-2video.mp4'
         cmd = 'ffmpeg -y -i {} -ss {} -t {} {}~{}.mp4'.format(
             video,
-            start_cut, (start_cut + N*60), 
+            start_cut, N*60, 
             start_cut,  (start_cut + N*60))
         print(cmd)
         os.system(cmd)
@@ -30,7 +31,7 @@ def get_N_Min_Video(N, video):
 
 
 if __name__ == '__main__':
-    N = 10
+    N = 20
     video_path = os.path.join(os.path.dirname(__file__), '301.mp4')
     get_N_Min_Video(N, video_path)
 
