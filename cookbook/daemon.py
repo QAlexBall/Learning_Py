f11c926aedc7df11075e4b6b37806a173ed45dfa
qalexball <zhuderen12345@icloud.com>
2019-12-08 22:25:43 +0800

    [update] add daemon.py

---
+++ cookbook/daemon.py
@@ -0,0 +1,89 @@
+import os
+import sys
+
+import atexit
+import signal
+
+def daemonize(pidfile, *, stdin='/dev/null',
+                          stdout='/dev/null',
+                          stderr='/dev/null'):
+    if os.path.exists(pidfile):
+        raise RuntimeError('Already running')
+
+    # First fork (detaches from parent)
+    try:
+        if os.fork() > 0:
+            raise SystemExit(0) # Parent exit
+    except OSError as e:
+        raise RuntimeError('fork #1 failed.', e)
+
+    os.chdir('/')  # change the current working directory to path
+    os.umask(0)    # 设置当前数值掩码，并返回之前的掩码
+    os.setsid()    # 设置当前进程的用户id
+    
+    # Second fork (relinquish session leadership)
+    try:
+        if os.fork() > 0:
+            raise SystemExit(0)
+    except OSError as e:
+        raise RuntimeError('fork #2 failed', e)
+
+    # Flush I/O buffers
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    # Replace file descriptors for stdin, stdout, and stderr
+    with open(stdin, 'rb', bufferring=0) as f:  # bufferring是一个可选的整数，用于设置缓冲策略。传递0以切换缓冲关闭（仅允许在而精致模式下）。
+        os.dup2(f.fileno(), sys.stdin.fileno())
+    with open(stdout, 'ab', 0) as f:
+        os.dup2(f.fileno(), sys.stdout.fileno())
+    with open(stderr, 'ab', 0) as f:
+        os.dup2(f.fileno(), sys.stderr.fileno())
+
+    # Write the PID file
+    with open(pidfile, 'w') as f:
+        print(os.getpid(), file=f)
+    
+    # Arrange to have the PID file removed on exit/signal
+    atexit.register(lambda: os.remove(pidfile))
+
+    # Sinal handler for termination (required)
+    def sigterm_handler(signo, frame):
+        raise SystemExit(1)
+
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+
+def main():
+    import time
+    sys.stdout.write('Daemon started with pid {}\n'.format(os.getpid()))
+    while True:
+        sys.stdout.write('Daemon Alive! {}\n'.format(time.ctime()))
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    PIDFILE = '/tmp/daemon.pid'
+    if len(sys.argv) != 2:
+        print('Usage: {} [start|stop]'.format(sys.argv[0]), file=sys.stderr)
+        raise SystemExit(1)
+
+    if sys.argv[1] == 'start':
+        try:
+            daemonize(PIDFILE, stdout='/tmp/daemon.log', stderr='/tmp/daemon.log')
+        except RuntimeError as e:
+            print(e, file=sys.stderr)
+            raise SystemExit(1)
+    
+        main()
+    elif sys.argv[1] == 'stop':
+        if os.path.exists(PIDFILE):
+            with open(PIDFILE) as f:
+                os.kill(int(f.read()), signal.SIGTERM)
+        else:
+            print('Not running', file=sys.stderr)
+            raise SystemExit(1)
+
+    else:
+        print('Unknown command {!r}'.format(sys.argv[1]), file=sys.stderr)
+        raise SystemExit(1)
