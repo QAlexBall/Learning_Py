9e55d3e7f3b818f167fd6b7036c7152039ca2e95
qalexball <zhuderen12345@icloud.com>
2020-05-17 01:05:06 +0800

    thespian example

---
+++ Three_Part_Moudule/Thespian/examples/hello_world.py
@@ -0,0 +1,13 @@
+from thespian.actors import *
+
+
+class Hello(Actor):
+    
+    def receiveMessage(self, msg, sender):
+        self.send(sender, 'Hello, World!')
+
+
+if __name__ == "__main__":
+    hello = ActorSystem().createActor(Hello)
+    print(ActorSystem().ask(hello, 'hi', 1))
+    ActorSystem().tell(hello, ActorExitRequest())
